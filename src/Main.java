b7cefa1b3acbaf408cbd5a1551f8a1ecdedb7436
Anastasiya <kabakova.nas@gmail.com>
2023-01-31 23:44:32 +0300

    "edit Main.java"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // объявление стоимости билета
+        double Cost = 7890.99;
+        // количетсво рублей для одной бонусной мили
+        int RubOneMile = 20;
+
+        // Рассчёт количества бонусных миль
+        int BonusMile = (int)Cost / RubOneMile;
+        // вывод реузльтата
+        System.out.println(BonusMile  + " - Количество бонусных миль.");
+
     }
 }
