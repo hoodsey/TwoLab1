0d8718cff6d1725e463e6504dbbc823e76411877
Nasya Kabakova <kabakova.nas@gmail.com>
2023-02-13 18:47:47 +0300

    Reformat Code

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,10 @@
 public class Main {
-    public static void main(String[] args) {
-
-        // объявление стоимости билета
-        double cost = 7890.99;
-        // количетсво рублей для одной бонусной мили
+    public static void main(String[] args) {// объявление стоимости билета
+        double cost = 7890.99;// количетсво рублей для одной бонусной мили
         int rubOneMile = 20;
+        int bonusMile = (int) cost / rubOneMile;// Рассчёт количества бонусных миль
 
-        // Рассчёт количества бонусных миль
-        int bonusMile = (int) cost / rubOneMile;
-        // вывод реузльтата
-        System.out.println(bonusMile + " - Количество бонусных миль.");
+        System.out.println(bonusMile + " - Количество бонусных миль."); // вывод реузльтата
 
     }
 }
